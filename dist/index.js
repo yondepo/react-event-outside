800bec8fb9817d7dc04a9641eedfa8a834a5b6b4
yondepo <yondepo@gmail.com>
2016-09-16 20:39:44 +0500

    builded

--- dist/index.js
+++ dist/index.js
@@ -12,10 +12,6 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = require('react-dom');
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -24,39 +20,64 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-exports.default = function (Target, events) {
-  return function (_React$Component) {
-    _inherits(ReactEventOutside, _React$Component);
+exports.default = function () {
+  var events = arguments.length <= 0 || arguments[0] === undefined ? ['click'] : arguments[0];
+  return function (Target) {
+    return function (_React$Component) {
+      _inherits(ReactEventOutside, _React$Component);
 
-    function ReactEventOutside(props) {
-      _classCallCheck(this, ReactEventOutside);
+      function ReactEventOutside(props) {
+        _classCallCheck(this, ReactEventOutside);
 
-      var _this = _possibleConstructorReturn(this, (ReactEventOutside.__proto__ || Object.getPrototypeOf(ReactEventOutside)).call(this, props));
+        var _this = _possibleConstructorReturn(this, (ReactEventOutside.__proto__ || Object.getPrototypeOf(ReactEventOutside)).call(this, props));
 
-      _this.handleEvent = _this.handleEvent.bind(_this);
-      return _this;
-    }
-
-    _createClass(ReactEventOutside, [{
-      key: 'componentDidMount',
-      value: function componentDidMount() {}
-    }, {
-      key: 'componentWillUnmount',
-      value: function componentWillUnmount() {}
-    }, {
-      key: 'handleEvent',
-      value: function handleEvent(e) {}
-    }, {
-      key: 'render',
-      value: function render() {
-        var _this2 = this;
-
-        return _react2.default.createElement(Target, _extends({}, this.props, { ref: function ref(node) {
-            return _this2.node = node;
-          } }));
+        _this.handleEvent = _this.handleEvent.bind(_this);
+        return _this;
       }
-    }]);
 
-    return ReactEventOutside;
-  }(_react2.default.Component);
+      _createClass(ReactEventOutside, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+          var _this2 = this;
+
+          events.forEach(function (event) {
+            return document.addEventListener(event, _this2.handleEvent, true);
+          });
+        }
+      }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+          var _this3 = this;
+
+          events.forEach(function (event) {
+            return document.removeEventListener(event, _this3.handleEvent, true);
+          });
+        }
+      }, {
+        key: 'handleEvent',
+        value: function handleEvent(e) {
+          if ((!this.wrap || !this.wrap.contains(e.target)) && typeof this.target.handleEvent === 'function') {
+            this.target.handleEvent(e);
+          }
+        }
+      }, {
+        key: 'render',
+        value: function render() {
+          var _this4 = this;
+
+          return _react2.default.createElement(
+            'div',
+            { ref: function ref(node) {
+                return _this4.wrap = node;
+              } },
+            _react2.default.createElement(Target, _extends({}, this.props, { ref: function ref(node) {
+                return _this4.target = node;
+              } }))
+          );
+        }
+      }]);
+
+      return ReactEventOutside;
+    }(_react2.default.Component);
+  };
 };
